5d67d2e37ba052bdc29ed3ccb71c4f33244aafa9
Henrique Tamashiro <henrique.tamashiro@outlook.com>
2024-09-26 23:59:24 +0000

    update

--- js/main.js
+++ js/main.js
@@ -51,41 +51,57 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
   });
 });
 
-// O restante do código não precisa de mudanças significativas
+  document.addEventListener('DOMContentLoaded', async () => {
+    const headerResponse = await fetch('/pages/header.html');
+    const headerData = await headerResponse.text();
+    document.getElementById('header-placeholder').innerHTML = headerData;
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const headerResponse = await fetch('./pages/header.html');
-  const headerData = await headerResponse.text();
-  document.getElementById('header-placeholder').innerHTML = headerData;
+    const token = localStorage.getItem('token');
+    const profileMenu = document.getElementById('profileMenu');
+    const loginButton = document.getElementById('loginButton');
+    const logoutButton = document.getElementById('logoutButton');
+    const dashboard = document.getElementById('dashboard');
 
-  const token = localStorage.getItem('token'); 
-  const profileMenu = document.getElementById('profileMenu');
-  const loginButton = document.getElementById('loginButton');
-  const dashboard = document.getElementById('dashboard');
-  
-  if (token) {
-      try {
-          const response = await fetch('http://34.207.139.134:3300/dashboard', {
-              method: 'GET',
-              headers: {
-                  'Authorization': `Bearer ${token}`
-              }
-          });
+    if (token) {
+        try {
+            const response = await fetch('http://34.207.139.134:3300/isLoggedIn', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
 
-          if (!response.ok) {
-              throw new Error('Token inválido ou expirado.');
-          }
+            const result = await response.json();
+         
+            if (response.ok) {
+                dashboard.innerText = result.username;
+                profileMenu.style.display = 'block';
+                loginButton.style.display = 'none';
+                logoutButton.style.display = 'block';
+            } else {
+                throw new Error('Token inválido ou expirado.');
+            }
 
-          const result = await response.json();
-          dashboard.innerText = result.message;
-          profileMenu.style.display = 'block';
-          loginButton.style.display = 'none';
+        } catch (error) {
+            console.error('Erro:', error);
+            alert('Sua sessão expirou. Faça login novamente.');
+            localStorage.removeItem('token');
+            window.location.replace('/pages/login.html');
 
-      } catch (error) {
-          console.error('Erro:', error);
-          alert('Sua sessão expirou. Faça login novamente.');
-          localStorage.removeItem('token');
-          window.location.href = 'login.html';
-      }
+           
+        }
+    } else {
+        profileMenu.style.display = 'none';
+        loginButton.style.display = 'block';
+        logoutButton.style.display = 'none';
     }
-  });
+
+
+    // Adicionar evento de logout
+    logoutButton.addEventListener('click', async () => {
+
+                localStorage.removeItem('token');
+                window.location.href = '/pages/login.html';
+
+    });
+});
